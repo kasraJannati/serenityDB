8bea8aa3b2a517705684cda0020f2cfbb8b106df
Kasra Jannati <kasra_ja1991@yahoo.com>
2022-01-27 23:05:28 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,3 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
-
-
-
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
@@ -24,10 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'postgres'), //postgres
       password: env('DATABASE_PASSWORD', 'admin'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
-      // ssl: env('DATABASE_SSL', false)
+      ssl: env('DATABASE_SSL', false)
     },
     debug: false,
   },
